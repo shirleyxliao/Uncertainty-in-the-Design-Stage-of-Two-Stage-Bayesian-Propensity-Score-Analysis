d7a2239a147962fb11171dbbbe5b4ea7b1cd59a3
shirleyxliao <shirleyxliao@gmail.com>
2019-09-28 16:28:12 -0400

    Add files via upload

--- DATA FILES/Figures code/p1_sim_graphs.R
+++ DATA FILES/Figures code/p1_sim_graphs.R
@@ -7,8 +7,11 @@ data.fxn = function(data){
   for(c in 1:ncolumns){
     if(c==1){
       returnframe = data.frame(data[,c])
+      returnframe$rep = 1
     } else{
-      returnframe = rbind(returnframe,data.frame(data[,c]))
+      xx = data.frame(data[,c])
+      xx$rep = c
+      returnframe = rbind(returnframe,xx)
     }
   }
   returnframe
@@ -17,45 +20,90 @@ data.fxn = function(data){
 #################################
 # read in results and restructure them
 
-load("~/Dropbox/Shirley's Dissertation/Paper1/DATA FILES/Results/BPSA.confound.R")
+load("/Users/shirleyliao/Dropbox/Shirley's Dissertation/Non-Github folder/BPSA paper/Draft 2/Results/BPSA.confound.R")
 all.methods = data.frame(data.type = "confound",data.fxn(bpsa.result))
 
-load("~/Dropbox/Shirley's Dissertation/Paper1/DATA FILES/Results/BPSA.instru.R")
+load("/Users/shirleyliao/Dropbox/Shirley's Dissertation/Non-Github folder/BPSA paper/Draft 2/Results/BPSA.instru.R")
 all.methods = rbind(all.methods,data.frame(data.type = "instru",data.fxn(bpsa.result)))
 
-load("~/Dropbox/Shirley's Dissertation/Paper1/DATA FILES/Results/BPSA.instru_prog.R")
+load("/Users/shirleyliao/Dropbox/Shirley's Dissertation/Non-Github folder/BPSA paper/Draft 2/Results/BPSA.instru_prog.R")
 all.methods = rbind(all.methods,data.frame(data.type = "instru_prog",data.fxn(bpsa.result)))
 
-load("~/Dropbox/Shirley's Dissertation/Paper1/DATA FILES/Results/BPSA.instru_prog_noise.R")
+load("/Users/shirleyliao/Dropbox/Shirley's Dissertation/Non-Github folder/BPSA paper/Draft 2/Results/BPSA.instru_prog_noise.R")
 all.methods = rbind(all.methods,data.frame(data.type = "instru_prog_noise",data.fxn(bpsa.result)))
 
-all.methods$sandwich_se = sqrt(all.methods$between.design.SE^2 + all.methods$within.design.sandwich.SE^2)
+#all.methods$sandwich_se = sqrt(all.methods$between.design.SE^2 + all.methods$within.design.sandwich.SE^2)
 
 all.methods$PROP = (all.methods$between.design.SE)^2/(all.methods$rubinse)^2
-all.methods$PROP_sandwich = (all.methods$between.design.SE)^2/(all.methods$sandwich_se)^2
+all.methods$interval.width = all.methods$interval.upper - all.methods$interval.lower
+#all.methods$PROP_sandwich = (all.methods$between.design.SE)^2/(all.methods$sandwich_se)^2
 
-all.methods$lower = all.methods$ATE - 1.96*all.methods$rubinse
-all.methods$upper = all.methods$ATE + 1.96*all.methods$rubinse
+#all.methods$interval.lower = all.methods$ATE - 1.96*all.methods$rubinse
+#all.methods$upper = all.methods$ATE + 1.96*all.methods$rubinse
 
-all.methods$lower_sandwich = all.methods$ATE - 1.96*all.methods$sandwich_se
-all.methods$upper_sandwich = all.methods$ATE + 1.96*all.methods$sandwich_se
+#all.methods$interval.lower_sandwich = all.methods$ATE - 1.96*all.methods$sandwich_se
+#all.methods$upper_sandwich = all.methods$ATE + 1.96*all.methods$sandwich_se
 
-all.methods$covered = as.numeric(1.5<all.methods$upper & 1.5>all.methods$lower)
-all.methods$covered_sandwich = as.numeric(1.5<all.methods$upper_sandwich & 1.5>all.methods$lower_sandwich)
+all.methods$covered = as.numeric(1.5<all.methods$interval.upper & 1.5>all.methods$interval.lower)
+#all.methods$covered_sandwich = as.numeric(1.5<all.methods$upper_sandwich & 1.5>all.methods$lower_sandwich)
 all.methods$bias = all.methods$ATE - 1.5
 
-save(all.methods,file="~/Dropbox/Shirley\'s Dissertation/Paper1/DATA FILES/Test code/scenario_1_sims_result.R")
+save(all.methods,file="/Users/shirleyliao/Dropbox/Shirley's Dissertation/Non-Github folder/BPSA paper/Draft 2/Test code/scenario_1_sims_result.R")
+
+results = subset(all.methods,select = c(rep,data.type,ATE,between.design.SE,
+                                          within.design.SE,method,rubinse,interval.lower,
+                                          interval.upper,covered,bias))
+results$interval.width = results$interval.upper - results$interval.lower
+results$model = "BPSA"
+
+load("/Users/shirleyliao/Dropbox/Shirley's Dissertation/Non-Github folder/BPSA paper/Draft 2/Results/PSA.confound.R")
+results.psa = data.frame(data.type = "confound",data.fxn(psa.result))
+
+load("/Users/shirleyliao/Dropbox/Shirley's Dissertation/Non-Github folder/BPSA paper/Draft 2/Results/PSA.intru.R")
+results.psa = rbind(results.psa,data.frame(data.type = "instru",data.fxn(psa.result)))
+
+load("/Users/shirleyliao/Dropbox/Shirley's Dissertation/Non-Github folder/BPSA paper/Draft 2/Results/PSA.intru_prog.R")
+results.psa = rbind(results.psa,data.frame(data.type = "instru_prog",data.fxn(psa.result)))
+
+load("/Users/shirleyliao/Dropbox/Shirley's Dissertation/Non-Github folder/BPSA paper/Draft 2/Results/PSA.intru_prog_noise.R")
+results.psa = rbind(results.psa,data.frame(data.type = "instru_prog_noise",data.fxn(psa.result)))
+
+xx = data.frame(rep = results.psa$rep,data.type = results.psa$data.type, ATE = results.psa$ATE, between.design.SE = 0,  
+                within.design.SE = results.psa$se, method = results.psa$method,rubinse = results.psa$se,
+                interval.lower = results.psa$ATE - 1.96*results.psa$se, 
+                interval.upper = results.psa$ATE + 1.96*results.psa$se,
+                #covered = ((interval.lower = results.psa$ATs) < 1.5 & (interval.upper) > 1.5 ),
+                bias = results.psa$ATE - 1.5,
+                interval.width = 2*1.96*results.psa$se,model = "PSA")
+xx$covered = (xx$interval.lower < 1.5 & xx$interval.upper > 1.5)
+
+results = rbind(results,xx)
+#results = xx
+#save(results,file="/Users/shirleyliao/Dropbox/Shirley's Dissertation/Non-Github folder/BPSA paper/Draft 3/Code/psa_results.R")
+save(results,file="/Users/shirleyliao/Dropbox/Shirley's Dissertation/Non-Github folder/BPSA paper/Draft 3/Code/interval_widths.R")
+
+#####################################
+
+#load("/Users/shirleyliao/Dropbox/Shirley's Dissertation/Non-Github folder/BPSA paper/Draft 3/Code/interval_widths.R")
+
+
+#xx = aggregate(interval.width~data.type+method+model,results,mean)
+
+#xx[order(xx$data.type,xx$method),]
+
+
+
 
 ################################################################
 ### code for table B1
 
 library(ggplot2)
-load("~/Dropbox/Shirley\'s Dissertation/Paper1/DATA FILES/Test code/scenario_1_sims_result.R")
-
+load("/Users/shirleyliao/Dropbox/Shirley's Dissertation/Non-Github folder/BPSA paper/Draft 2/Test code/scenario_1_sims_result.R")
+#all.methods = results
 #all.methods = subset(all.methods,data.type %in% c("confound","instru"))
 
 all.methods$covered = as.character(all.methods$covered)
-ggplot(all.methods,aes(x = data.type,y=PROP,color=covered)) + geom_jitter() + facet_wrap(~method)
+#ggplot(all.methods,aes(x = data.type,y=PROP,color=covered)) + geom_jitter() + facet_wrap(~method)
 
 all.methods$covered = as.numeric(all.methods$covered)
 all.methods$Var.ATE = all.methods$rubinse^2
@@ -64,38 +112,96 @@ all.methods$Var.within = all.methods$within.design.SE^2
 
 xx = aggregate(ATE~data.type+method,all.methods,var)
 
-xx = merge(xx,aggregate(Var.ATE ~ method+data.type,all.methods,mean),by=c('method',
+xx = merge(xx,aggregate(Var.ATE ~ method+data.type,all.methods,mean),by=c('method','data.type',
                                                                             'data.type'))
-xx = merge(xx,aggregate(Var.between ~ method+data.type,all.methods,mean),by=c('method',
+xx = merge(xx,aggregate(Var.between ~ method+data.type,all.methods,mean),by=c('method','data.type',
                                                                           'data.type'))
-xx = merge(xx,aggregate(Var.within ~ method+data.type,all.methods,mean),by=c('method',
+xx = merge(xx,aggregate(Var.within ~ method+data.type,all.methods,mean),by=c('method','data.type',
                                                                           'data.type'))
-xx = merge(xx,aggregate(bias ~ method+data.type,all.methods,mean),by=c('method',
+xx = merge(xx,aggregate(bias ~ method+data.type,all.methods,mean),by=c('method','data.type',
                                                                        'data.type'))
-xx = merge(xx,aggregate(PROP ~ method+data.type,all.methods,mean),by=c('method',
+xx = merge(xx,aggregate(PROP ~ method+data.type,all.methods,mean),by=c('method','data.type',
                                                                        'data.type'))
-xx = merge(xx,aggregate(covered ~ method+data.type,all.methods,mean),by=c('method',
+xx = merge(xx,aggregate(covered ~ method+data.type,all.methods,mean),by=c('method','data.type',
                                                                           'data.type'))
 mse = function(x){mean((x-1.5)^2)}
 
-xx = merge(xx,aggregate(ATE ~ method+data.type,all.methods,mse),by=c('method',
+xx = merge(xx,aggregate(ATE ~ method+data.type,all.methods,mse),by=c('method','data.type',
                                                                           'data.type'))
+
+xx = merge(xx,aggregate(interval.width~method+data.type,all.methods,mean),by=c('method','data.type'))
 #names(xx) = c('method','data.type','empirical.var','avg.total.var','avg.between.var','avg.within.var','bias','PROP','coverage')
 names(xx) = c('Implementation','PS model','Empirical var',
               'Avg total var','Avg between var','Avg within var','Bias','PROP_DU','Coverage',
-              'MSE')
+              'MSE','Interval width')
 
-xx
+#xx
 
 library(xtable)
 xtable(xx,digits=3)
 
-########################################################
-# Figure 3
+#######################################################
+# Table B2
+load("/Users/shirleyliao/Dropbox/Shirley's Dissertation/Non-Github folder/BPSA paper/Draft 3/Code/psa_results.R")
 
-tiff("/Users/shirleyliao/Dropbox/Shirley's Dissertation/Paper1/Figures/bias.tiff")
-ggplot(all.methods,aes(x=PROP,y=bias,color=method)) + geom_point()+ggtitle("Bias")
+results$Var.ATE = results$within.design.SE^2
+bias = 1.5
+
+xx = aggregate(ATE~method+data.type,results,var)
+
+xx = merge(xx,aggregate(Var.ATE ~ method+data.type,results,mean),by=c('method','data.type'))
+
+xx = merge(xx,aggregate(bias ~ method+data.type,results,mean),by=c('method','data.type'))
+xx = merge(xx,aggregate(covered ~ method+data.type,results,mean),by=c('method','data.type'))
+mse = function(x,bias){mean((x-bias)^2)}
+xx = merge(xx,aggregate(ATE ~ method+data.type,results,mse,bias=bias),by=c('method','data.type'))
+xx = merge(xx,aggregate(interval.width~method+data.type,results,mean),by=c('method','data.type'))
+
+names(xx) = c('Implementation','PS model','Empirical var',
+              'Avg total var','Bias','Coverage',
+              'MSE','Interval width')
+
+library(xtable)
+xtable(xx,digits=3)
+
+#####################
+
+#interval widths figures (Figures B1-B3)
+
+load("~/Dropbox/Shirley's Dissertation/Non-Github folder/BPSA paper/Draft 3/Code/interval_widths.R")
+
+metric = "bias"
+xx = eval(parse(text = paste("aggregate(", metric, "~data.type+method+model,results,mean)", sep="")))
+
+#tiff("/Users/shirleyliao/Dropbox/Shirley's Dissertation/Paper1/Figures/bias_plot.tiff")
+png("/Users/shirleyliao/Dropbox/Shirley's Dissertation/Paper1/Figures/bias_plot.png")
+ggplot(xx,aes(x=data.type,y=bias,color=model))+
+  geom_point()+facet_wrap(.~method)+geom_hline(yintercept=0)+
+  labs(title="Bias under PSA/BPSA",x="Implementation",y="Bias") + 
+  theme(axis.text.x = element_text(angle = 45))
 dev.off()
+
+metric = "covered"
+xx = eval(parse(text = paste("aggregate(", metric, "~data.type+method+model,results,mean)", sep="")))
+
+png("/Users/shirleyliao/Dropbox/Shirley's Dissertation/Paper1/Figures/coverage_plot.png")
+ggplot(xx,aes(x=data.type,y=covered,color=model))+
+  geom_point()+facet_wrap(.~method)+geom_hline(yintercept=0.95)+
+  labs(title="Coverage of 95% CI under PSA/BPSA",x="Implementation",y="Proportion coverage")+ 
+  theme(axis.text.x = element_text(angle = 45))
+dev.off()
+
+metric = "mse"
+results$mse = (results$bias)^2 + results$rubinse^2
+xx = eval(parse(text = paste("aggregate(", metric, "~data.type+method+model,results,mean)", sep="")))
+
+png("/Users/shirleyliao/Dropbox/Shirley's Dissertation/Paper1/Figures/mse_plot.png")
+ggplot(xx,aes(x=data.type,y=mse,color=model))+
+  geom_point()+facet_wrap(.~method)+
+  labs(title="MSE under PSA/BPSA",x="Implementation",y="MSE")+ 
+  theme(axis.text.x = element_text(angle = 45))
+dev.off()
+
 
 ######################################################
 # Unused figures
@@ -130,5 +236,10 @@ all.methods$log_var.within = log(all.methods$Var.within)
 tiff("/Users/shirleyliao/Dropbox/Shirley's Dissertation/Paper1/Figures/within_var.tiff")
 ggplot(all.methods,aes(x=PROP,y=log_var.within,color=method)) + geom_point()+ggtitle("Within design variance")
 dev.off()
+########################################################
+# Figure 3
 
+tiff("/Users/shirleyliao/Dropbox/Shirley's Dissertation/Paper1/Figures/bias.tiff")
+ggplot(all.methods,aes(x=PROP,y=bias,color=method)) + geom_point()+ggtitle("Bias")
+dev.off()
 #######################
